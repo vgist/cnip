41cc8d98278390e22bf4de7041bf22f364b81dd2
clown <458892+aieu@users.noreply.github.com>
2025-05-22 12:59:13 +0800

    drop zone files

--- cnip-update.sh
+++ cnip-update.sh
@@ -4,22 +4,28 @@
 
 set -ex
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TMP_DIR="$(mktemp -d)"
+
 echo "Downloading cnv4 ip..."
-curl -sSL https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone -o /tmp/cnv4.zone
+curl -sSL "https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv4.zone"
 echo "Downloading cnv6 ip..."
-curl -sSL https://www.ipdeny.com/ipv6/ipaddresses/aggregated/cn-aggregated.zone -o /tmp/cnv6.zone
+curl -sSL "https://www.ipdeny.com/ipv6/ipaddresses/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv6.zone"
 
-_path=`pwd`
-
-for i in cnv4 cnv6; do
-    if ! diff /tmp/$i.zone $_path/$i.zone >/dev/null; then
-        echo "Creating new $i.nft..."
-        cp -f /tmp/$i.zone $_path/$i.zone
-        echo "define _"$i"_list = {" > $_path/$i.nft
-        sed "s/^/  /g; s/$/,/g" $_path/$i.zone >> $_path/$i.nft
-        echo "}" >> $_path/$i.nft
+for ip_type in cnv4 cnv6; do
+    echo "Creating new $ip_type.nft..."
+    tmp_file="$TMP_DIR/${ip_type}.nft"
+    dst_file="$SCRIPT_DIR/${ip_type}.nft"
+    echo "define _${ip_type}_list = {" > "$tmp_file"
+    sed 's/^/  /; s/$/,/' "$TMP_DIR/${ip_type}.zone" >> "$tmp_file"
+    echo "}" >> "$tmp_file"
+    if [ ! -f "$dst_file" ] || ! cmp -s "$tmp_file" "$dst_file"; then
+        cp -f "$tmp_file" "$dst_file"
+        echo "Updated $dst_file"
     else
-        echo "No need to update $i"
+        echo "No need to update $ip_type.nft"
     fi
-    rm /tmp/$i.zone
 done
+
+rm -rf "$TMP_DIR"
+echo "Done."
