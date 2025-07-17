a68082450fd6d8358461798797fdd3b6f8441b99
clown <458892+aieu@users.noreply.github.com>
2025-07-17 14:22:30 +0800

    data from DH-Teams

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 china ip v4/v6 segment for nftables sets
+
+data comes from https://github.com/DH-Teams/DH-Geo_AS_IP_CN
--- cnip-update.sh
+++ cnip-update.sh
@@ -8,9 +8,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TMP_DIR="$(mktemp -d)"
 
 echo "Downloading cnv4 ip..."
-curl -sSL "https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv4.zone"
+#curl -sSL "https://www.ipdeny.com/ipblocks/data/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv4.zone"
+curl -sSL "https://github.com/DH-Teams/DH-Geo_AS_IP_CN/raw/refs/heads/main/Geo_AS_IP_CN.txt" -o "$TMP_DIR/cnv4.zone"
 echo "Downloading cnv6 ip..."
-curl -sSL "https://www.ipdeny.com/ipv6/ipaddresses/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv6.zone"
+#curl -sSL "https://www.ipdeny.com/ipv6/ipaddresses/aggregated/cn-aggregated.zone" -o "$TMP_DIR/cnv6.zone"
+curl -sSL "https://github.com/DH-Teams/DH-Geo_AS_IP_CN/raw/refs/heads/main/Geo_AS_IP_CN_6.txt" -o "$TMP_DIR/cnv6.zone"
 
 for ip_type in cnv4 cnv6; do
     echo "Creating new $ip_type.nft..."
